4b1747d22e8c5246b6bc8d379a2ca88d8e6b8454
Saba Okriashvili <sabaaoqrish@gmail.com>
2023-02-08 12:10:13 +0400

    add new card button with functions

---
+++ script.js
@@ -0,0 +1,31 @@
+let firstCard = 6
+let secondCard = 8
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.querySelector("#cards-el")
+
+function startGame() {
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = `Cards: ${firstCard} ${secondCard}` 
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+function newCard() {
+    messageEl.textContent = "Drawing a new card from the deck!"
+    let card = 7
+    sum += card
+    startGame()
+}
